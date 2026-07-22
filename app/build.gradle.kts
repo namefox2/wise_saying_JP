bf6f28da9539b3b13f032983ba76e0c03d86545b
seonae <namefox2@naver.com>
2026-07-22 13:24:32 +0900

    version 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kotoba.takarabako"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.3.2"
+        versionCode = 16
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
